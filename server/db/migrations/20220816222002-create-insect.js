271b8bcbf8e45385f781c1f46fc2e30f7ddf00c6
Jason Kong <jrkong216@gmail.com>
2022-08-16 15:44:44 -0700

    first commit

---
+++ server/db/migrations/20220816222002-create-insect.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Insects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        isUppercase: true
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      territory: {
+        type: Sequelize.STRING
+      },
+      fact: {
+        type: Sequelize.STRING,
+        max: 240
+      },
+      millimeters: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        min: 0
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Insects');
+  }
+};
